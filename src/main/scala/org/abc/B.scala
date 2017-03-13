fcc27821ca9b3219085b53e9a3c20510aeb3b450
Ryan Williams <ryan.blake.williams@gmail.com>
2017-03-13 17:22:06 +0000

    add comment

--- src/main/scala/org/abc/B.scala
+++ src/main/scala/org/abc/B.scala
@@ -3,5 +3,9 @@ package org.abc
 trait B extends Comparable[B]
 
 object B {
+  /**
+   * Iff this line is compiled before [[C]] below, then [[C]] will throw a "divergent implicit
+   * expansion" error, though the compiler should not carry any state from here to there.
+   */
   implicitly[Ordering[B]]
 }
