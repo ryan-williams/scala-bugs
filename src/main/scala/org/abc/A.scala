7e01b86336bc222289034f398d0977b4a003d898
Ryan Williams <ryan.blake.williams@gmail.com>
2017-07-05 16:05:30 +0000

    move off of InputStream

--- src/main/scala/org/abc/A.scala
+++ src/main/scala/org/abc/A.scala
@@ -2,18 +2,16 @@ package org.abc
 
 import java.io.InputStream
 
+trait I
+
 trait A
-  extends InputStream {
-
-  override def read(): Int = 0
-
+  extends I {
   protected def foo(s: String): Unit
 }
 
 object A {
-  implicit class InputStreamA(is: InputStream)
+  implicit class InputStreamA(i: I)
     extends A {
-
     override protected def foo(s: String): Unit = ???
   }
 }
