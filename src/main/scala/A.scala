b573d7959a89a4bdd67f686574bfa3d0c548018f
Ryan Williams <ryan.blake.williams@gmail.com>
2019-06-19 18:14:24 +0000

    removing shapeless-specific types

--- src/main/scala/A.scala
+++ src/main/scala/A.scala
@@ -1,60 +1,49 @@
-import shapeless._
+import shapeless.{ the, Lazy }
 
-object traits {
-  // Type that provides functionality in terms of a provided HList type
-  // - Conforms to `Generic` contract in terms of the wrapped HList
-  // - Generic HNil instance is provided
-  sealed trait A[+L <: HList]
-  object A {
-    type ⟘ = A[HNil]
-    implicit val generic: Generic.Aux[A[HNil], HNil] = null
-  }
+// Stand-in for shapeless.Generic.Aux; theoretically easier to resolve because the Repr type-member is a type-param instead.
+trait G[T, Repr]
 
-  trait A2[+L <: HList]
-  object A2 {
-    type ⟘ = A2[HNil]
-    implicit val generic: Generic.Aux[A2[HNil], HNil] = null
-  }
+// Likewise, HList and HNil replacements, to rule out any possible special treatment of those types in Lazy derivations
+sealed trait HList
+trait HNil extends HList
+case object HNil extends HNil
 
-  trait A3
-  object A3 {
-    type ⟘ = A3
-    implicit val generic: Generic.Aux[A3, HNil] = null
-  }
-}
+// Example class with a simple "Generic" instance with Repr type HNil
+trait A; object A { implicit val g: G[A, HNil] = null }
 
-import traits.A3.⟘
+// Example output type to try to derive a TC instance for
+trait Out
 
 // Type-class mapping an input type to an output type. HNil ⇒ CNil instance is provided
 trait TC[In] { type Out }
 object TC {
   type Aux[In, Out_] = TC[In] { type Out = Out_ }
-  implicit def hnil[L <: HNil]: TC.Aux[L, CNil] = null
+  implicit val hnil: TC.Aux[HNil, Out] = null
 }
 
 // If `T` is generic, and its generic repr has a `TC`, provide a `TC` for `T` with the same `Out` type
 // Eager and Lazy versions are provided; the Lazy version does not work as expected
 object derive {
-  implicit def eager_[T, L <: HList](implicit g: Generic.Aux[T, L], tc:      TC[L]) : TC.Aux[T, tc      .Out] = null  // 👌🏻
-  implicit def lazy_ [T, L <: HList](implicit g: Generic.Aux[T, L], tc: Lazy[TC[L]]): TC.Aux[T, tc.value.Out] = null  // 🐉
+  implicit def eager_[T, L <: HList](implicit g: G[T, L], tc:      TC[L]) : TC.Aux[T, tc      .Out] = null  // 👌🏻
+  implicit def lazy_ [T, L <: HList](implicit g: G[T, L], tc: Lazy[TC[L]]): TC.Aux[T, tc.value.Out] = null  // 🐉
 }
 
 object testEager {
   import derive.eager_
-  the[TC    [⟘      ]]  // ✅
-  the[TC.Aux[⟘, CNil]]  // ✅
+  the[TC    [A     ]]  // ✅
+  the[TC.Aux[A, Out]]  // ✅
 }
 object testLazy {
   import derive.lazy_
-  the[TC    [⟘      ]]  // ✅
-  the[TC.Aux[⟘, CNil]]  // 🚫
+  the[TC    [A     ]]  // ✅
+  the[TC.Aux[A, Out]]  // 🚫
 }
 
 object console {
 import shapeless._
-{ import derive.eager_; the[TC    [⟘      ]]                  }  // ✅
-{ import derive.eager_; the[TC.Aux[⟘, CNil]]                  }  // ✅
-{ import derive.lazy_ ; the[TC    [⟘      ]]                  }  // ✅
-{ import derive.lazy_ ; the[TC    [⟘      ]]: TC.Aux[⟘, CNil] }  // ✅
-{ import derive.lazy_ ; the[TC.Aux[⟘, CNil]]             }  // 🚫
+{ import derive.eager_; the[TC    [A      ]]                 }  // ✅
+{ import derive.eager_; the[TC.Aux[A, Out]]                  }  // ✅
+{ import derive.lazy_ ; the[TC    [A      ]]                 }  // ✅
+{ import derive.lazy_ ; the[TC    [A      ]]: TC.Aux[A, Out] }  // ✅
+{ import derive.lazy_ ; the[TC.Aux[A, Out]]             }  // 🚫
 }
